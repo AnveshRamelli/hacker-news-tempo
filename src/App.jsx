103e697d35fa51da3447c92dab2ca284898acad4
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-06-03 10:45:03 +0530

    displayed top story titles by fetching hacker news api

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+
+const App = () => {
+  const [topStories, setTopStories] = useState([])
+  const BASE_URL = 'https://hacker-news.firebaseio.com/v0';
+const getTopStories = async () => {
+  const response = await fetch(`${BASE_URL}/topstories.json`);
+  const ids = await response.json();
+
+  // Fetch each item
+  const storyPromises = ids.map(id =>
+    fetch(`${BASE_URL}/item/${id}.json`).then(res => res.json())
+  );
+
+  const stories = await Promise.all(storyPromises);
+  setTopStories(stories); //  // array of story objects
+};
+
+  useEffect(() => {
+    getTopStories();
+  }, [])
+  
+
+  return ( topStories.length > 0 ?
+    <div className="p-4">
+      {topStories.map(story => (
+        <p key={story.id} className="my-2"> - {story.title}</p>
+      ))}
+    </div> : <div className="p-5 text-2xl">Loading...</div>
+  )
+}
+
+export default App
+
+
+// API docs: https://github.com/HackerNews/API
+// Endpoint for list of stories: https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
+// Endpoint for detail for a story /v0/item/<id>.json (the .json at the end is required, e.g. 
+//   https://hacker-news.firebaseio.com/v0/item/8863.json
